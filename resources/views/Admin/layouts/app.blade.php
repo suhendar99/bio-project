1f6ee22bb94dcb5206984f8952301687dee30087
suhendar99 <a.suhendar2001@gmail.com>
2020-06-23 10:51:39 +0700

    New

--- resources/views/Admin/layouts/app.blade.php
+++ resources/views/Admin/layouts/app.blade.php
@@ -198,14 +198,7 @@
                 <div class="container-fluid">
                     <div class="row" style="margin-left:250px;">
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                             Copyright © 2018 Concept. All rights reserved. Dashboard by <a href="https://colorlib.com/wp/">Colorlib</a>.
-                        </div>
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                            <div class="text-md-right footer-links d-none d-sm-block">
-                                <a href="javascript: void(0);">About</a>
-                                <a href="javascript: void(0);">Support</a>
-                                <a href="javascript: void(0);">Contact Us</a>
-                            </div>
+                             Copyright © 2018 Makerindo. All rights reserved
                         </div>
                     </div>
                 </div>

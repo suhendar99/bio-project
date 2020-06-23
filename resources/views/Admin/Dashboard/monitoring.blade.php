b2109d593dd093b1394112e5e85dbe79a6ca8aca
suhendar99 <a.suhendar2001@gmail.com>
2020-06-23 18:16:47 +0700

    new

--- resources/views/Admin/Dashboard/monitoring.blade.php
+++ resources/views/Admin/Dashboard/monitoring.blade.php
@@ -1,17 +1,10 @@
 @php
     $mqtt = \App\Mqtt::where('id',1)->first();
     $app = \App\Setapp::where('id',1)->first();
-
-<<<<<<< HEAD
     $suhu = \App\Satuan::where('id',2)->first();
     $kelembapan = \App\Satuan::where('id',3)->first();
     $tekanan = \App\Satuan::where('id',4)->first();
-
-    $mon = \App\Monitoring::orderBy('id_monitoring', 'desc')->limit(10)->get();
-    $monitoring = $mon->sort();
-=======
     $monitoring = \App\Monitoring::orderBy('id_monitoring', 'asc')->limit(10)->get();
->>>>>>> 0c771ade0287a851e9169a0f3fdf42ca26607ec4
 @endphp
 
  <?php

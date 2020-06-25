29fced8956e929e68c962ff23a1e5467e8491f1d
apadilah30 <aguspadilah30@gmail.com>
2020-06-25 10:16:54 +0700

    aktivasi perangka

--- resources/views/Admin/Dashboard/alarm.blade.php
+++ resources/views/Admin/Dashboard/alarm.blade.php
@@ -5,22 +5,7 @@
                     <section class="cd-timeline js-cd-timeline">
                         <div class="cd-timeline__container" id="alarmReal">
                             <!-- cd-timeline__block -->
-                            @foreach($alarm as $a)
-                            <div class="cd-timeline__block js-cd-block">
-                                <div class="cd-timeline__img cd-timeline__img--movie js-cd-img">
-                                    <img src="{{ asset('svg/alarm.svg') }}" alt="Movie" width="40px" height="40px">
-                                </div>
-                                <!-- cd-timeline__img -->
-                                <div class="cd-timeline__content js-cd-content">
-                                    <h3>Alarm 1</h3>
-                                    <p>Alarm ON : {{ $a->time }}</p>
-                                    <p>Status : {{ \App\Log_alert::where('monitoring_id', $a->id_monitoring)->first()->status }}</p>
-                                    <p>Keterangan : {{ \App\Log_alert::where('monitoring_id', $a->id_monitoring)->first()->keterangan }}</p>
-                                    <span class="cd-timeline__date">{{ $a->date }}</span>
-                                </div>
-                                <!-- cd-timeline__content -->
-                            </div>
-                            @endforeach
+                            
                         </div>
                     </section>
                     <!-- cd-timeline -->

86303351b6c214260dd8c2717aa704e9f539fbab
suhendar99 <a.suhendar2001@gmail.com>
2020-07-24 06:03:45 +0700

    fix footer

--- resources/views/Admin/layouts/app.blade.php
+++ resources/views/Admin/layouts/app.blade.php
@@ -211,7 +211,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                             Copyright © 2020 Makerindo. All rights reserved
+                             Copyright © 2020 Makerindo. All rights reserve
                         </div>
                     </div>
                 </div>

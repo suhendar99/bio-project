a7174ec1380767da56c7eba22d1d175e70098aa2
faliq_14 <faliq.sawala.space>
2020-07-24 06:54:26 +0700

    fix update and delete foto operator & ruangan

--- app/Http/Controllers/RuanganController.php
+++ app/Http/Controllers/RuanganController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Ruangan;
 use Validator;
+use Illuminate\Support\Facades\File;
 
 class RuanganController extends Controller
 {
@@ -39,12 +40,9 @@ class RuanganController extends Controller
             if ($request->smax < $request->smin || $request->kmax < $request->kmin || $request->tmax < $request->tmin) {
                 return back()->with('failed', 'Parameter Maksimum Harus Lebih Besar Dari Minimum');
             }
-            if($request->hasfile('foto'))
-            {                
-                $name = rand(). '.' . $request->foto->getClientOriginalExtension();           
-                $request->foto->move(public_path("foto/ruangan"), $name);                                       
-                $foto = 'foto/ruangan/'.$name;
-            }
+
+            $name = time().'-'.$request->foto->getClientOriginalExtension();  
+            $request->foto->move(public_path("foto/ruangan"), $name);          
 
             $employee = Ruangan::create([
                 'nama' => $request->nama,
@@ -79,16 +77,30 @@ class RuanganController extends Controller
 
     public function update(Request $request, $id)
     {
-        $v = Validator::make($request->all(), [             
-            'nama' => 'required|',            
-            'foto' => 'image|mimes:jpeg,png,jpg|max:2048',
-            'smax' => 'required|',
-            'smin' => 'required|',
-            'kmax' => 'required|',
-            'kmin' => 'required|',
-            'tmax' => 'required|',
-            'tmin' => 'required|',
-        ]);
+        if ($request->file('foto')) {
+            $v = Validator::make($request->all(), [             
+                'nama' => 'required|',            
+                'foto' => 'image|mimes:jpeg,png,jpg|max:2048',
+                'smax' => 'required|',
+                'smin' => 'required|',
+                'kmax' => 'required|',
+                'kmin' => 'required|',
+                'tmax' => 'required|',
+                'tmin' => 'required|',
+            ]);
+        } else {
+            $v = Validator::make($request->all(), [             
+                'nama' => 'required|',            
+                'smax' => 'required|',
+                'smin' => 'required|',
+                'kmax' => 'required|',
+                'kmin' => 'required|',
+                'tmax' => 'required|',
+                'tmin' => 'required|',
+            ]);
+        }
+        
+        
 
         if ($v->fails()) {
             return back()->withErrors($v)->withInput();
@@ -97,32 +109,39 @@ class RuanganController extends Controller
                 return back()->with('failed', 'Parameter Maksimum Harus Lebih Besar Dari Minimum');
             }
             $ruangan = Ruangan::find($id);
-            
-            $ruangan->update([
-                'nama' => $request->nama,
-                'smax' => $request->smax,
-                'smin' => $request->smin,
-                'kmax' => $request->kmax,
-                'kmin' => $request->kmin,
-                'tmax' => $request->tmax,
-                'tmin' => $request->tmin,
-            ]);
 
-            if($request->hasfile('foto'))
-            {                
-                $name = rand(). '.' . $request->foto->getClientOriginalExtension();
+            if ($request->file('foto')) {
                 File::delete($ruangan->foto);
+
+                $foto = $request->file('foto');
+                $name = time().'-'. $foto->getClientOriginalName();
                 $request->foto->move(public_path("foto/ruangan"), $name);                                       
-                $foto = 'foto/ruangan/'.$name;
-                if(is_writable(public_path($ruangan->foto))) {                    
-                    unlink(public_path($ruangan->foto));
-                }     
-                $foto = 'foto/ruangan/'.$name;
+                // if(is_writable(public_path($ruangan->foto))) {                    
+                //     unlink(public_path($ruangan->foto));
+                // }     
 
                 $ruangan->update([
-                    'foto' => $foto,
+                    'nama' => $request->nama,
+                    'foto' => 'foto/ruangan/'.$name,
+                    'smax' => $request->smax,
+                    'smin' => $request->smin,
+                    'kmax' => $request->kmax,
+                    'kmin' => $request->kmin,
+                    'tmax' => $request->tmax,
+                    'tmin' => $request->tmin,
                 ]);
-            }            
+            } else {
+                $ruangan->update([
+                    'nama' => $request->nama,
+                    'smax' => $request->smax,
+                    'smin' => $request->smin,
+                    'kmax' => $request->kmax,
+                    'kmin' => $request->kmin,
+                    'tmax' => $request->tmax,
+                    'tmin' => $request->tmin,
+                ]);
+            }
+             
             // dd($ruangan);
             if ($ruangan) {
                 return back()->with('success', 'Data ruangan berhasil di update');
@@ -141,11 +160,7 @@ class RuanganController extends Controller
     public function destroy($id)
     {
         $ruangan = Ruangan::find($id);
-        if ($ruangan->foto) {            
-            if (is_writable(public_path($ruangan->foto))) {
-                unlink(public_path($ruangan->foto));
-            }
-        }
+        File::delete($ruangan->foto);
         $ruangan->delete();
         return back();
     }
